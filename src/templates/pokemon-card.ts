d16001e0fbbe9f91e6ca4c9b6c6fc7c166319e96
Andre Funk <andre.funk@icloud.com>
2026-02-03 21:08:44 +0100

    style: pokemon list finish

--- src/templates/pokemon-card.ts
+++ src/templates/pokemon-card.ts
@@ -1,14 +1,24 @@
 import type { PokemonModel } from "../models/pokemon.model";
+import { getTypesTemplate } from "./pokemon-type";
 
 export function getCardTemplate (data: PokemonModel){
-    return `
-    <article class="card">
+  let typesHTML = ""
+  const mainType = data.getTypes()[0]
+  
+  for (const type of data.getTypes()) {
+    typesHTML += getTypesTemplate(type)
+  }  
+
+  return `
+    <article class="card type-${mainType}">
       <div class="card__header">
         <p>#${data.getId()}</p>
         <h1>${data.getName()}</h1>
       </div>
       <img class="card__image" src="${data.getImg()}" alt="${data.getName()} image">
-      <div class="card__types" data-pokemon-types></div>
+      <div class="card__types">
+        ${typesHTML}
+      </div>
     </article>
     `
 }
